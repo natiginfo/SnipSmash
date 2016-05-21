b3d72d4e5d02d307475d9b863dc87f7efd32bdf4
Adrian Spanu <adri@nspanu.ca>
2016-05-21 13:06:01 +0200

    Adding SnipListActivity

---
+++ app/src/main/java/contafe/snipsmash/activities/SnipListActivity.java
@@ -0,0 +1,15 @@
+package contafe.snipsmash.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import contafe.snipsmash.R;
+
+public class SnipList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snip_list);
+    }
+}
