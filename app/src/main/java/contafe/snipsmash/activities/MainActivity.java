cfc12ecfc342ed3fe986af632aa9b022fe1e8227
Natig <babayev96@bk.ru>
2016-05-21 12:41:22 +0200

    Project files

--- app/src/main/java/contafe/snipsmash/activities/MainActivity.java
+++ app/src/main/java/contafe/snipsmash/activities/MainActivity.java
@@ -2,10 +2,16 @@ package contafe.snipsmash.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import contafe.snipsmash.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private EditText usernameET;
+    private EditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
