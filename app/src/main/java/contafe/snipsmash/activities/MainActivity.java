7327a8c452142ca268cf411a727180ab1b1eb2e2
Natig <babayev96@bk.ru>
2016-05-21 15:10:04 +0200

    Checking if username and password right

--- app/src/main/java/contafe/snipsmash/activities/MainActivity.java
+++ app/src/main/java/contafe/snipsmash/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package contafe.snipsmash.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -26,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private Button loginButton;
     private EditText usernameET;
     private EditText passwordET;
+    private ProgressBar loginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginProgress = (ProgressBar) findViewById(R.id.loginProgressBar);
         loginButton = (Button) findViewById(R.id.loginButton);
         usernameET = (EditText) findViewById(R.id.usernameEditText);
         passwordET = (EditText) findViewById(R.id.passwordEditText);
@@ -62,21 +66,31 @@ public class MainActivity extends AppCompatActivity {
             loginClient.post(MainActivity.this, Constants.API_URL + "/me/login", entity, "application/json", new AsyncHttpResponseHandler() {
                 @Override
                 public void onStart() {
-                    //posting started
+                    loginProgress.setVisibility(View.VISIBLE);
                 }
 
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    loginProgress.setVisibility(View.GONE);
+
                     String response = new String(responseBody);
+
                     try {
                         JSONObject jsonResponse = new JSONObject(response);
+                        if (!jsonResponse.has("error")) {
+                            tokenManager.setAccessToken(jsonResponse.getString("access_token"));
+                            tokenManager.setRefreshToken(jsonResponse.getString("refresh_token"));
+                            tokenManager.save();
 
-                        tokenManager.setAccessToken(jsonResponse.getString("access_token"));
-                        tokenManager.setRefreshToken(jsonResponse.getString("refresh_token"));
-                        tokenManager.save();
+                            System.out.println("RESPONSE: " + jsonResponse.toString());
+                            System.out.println("LOGGED IN: " + tokenManager.isLoggedIn());
 
-                        System.out.println("RESPONSE: " + jsonResponse.toString());
-                        System.out.println("LOGGED IN: " + tokenManager.isLoggedIn());
+                            Intent intent = new Intent(MainActivity.this, SnipListActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MainActivity.this, jsonResponse.getString("error_description"), Toast.LENGTH_LONG).show();
+                        }
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -84,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    loginProgress.setVisibility(View.GONE);
+                    Toast.makeText(MainActivity.this, "Something went wrong. Please retry again.", Toast.LENGTH_SHORT).show();
                     String response = new String(responseBody);
                     System.out.println("ERROR RESPONSE: " + responseBody);
                 }
