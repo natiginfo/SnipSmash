681723832f48c25e9fb0ea331381b8121a8d8d92
Adrian Spanu <adri@nspanu.ca>
2016-05-21 14:00:36 +0200

    Adding the SnipList Activity.

--- app/src/main/java/contafe/snipsmash/activities/SnipListActivity.java
+++ app/src/main/java/contafe/snipsmash/activities/SnipListActivity.java
@@ -3,13 +3,27 @@ package contafe.snipsmash.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 import contafe.snipsmash.R;
 
-public class SnipList extends AppCompatActivity {
+public class SnipListActivity extends AppCompatActivity {
+
+    private String userToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snip_list);
     }
+
+    protected JSONObject getFavouriteSnipList() {
+        JSONObject favSnipList = new JSONObject();
+
+
+
+        return favSnipList;
+    }
+
+
 }
--- app/src/main/java/contafe/snipsmash/defaults/Constants.java
+++ app/src/main/java/contafe/snipsmash/defaults/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     public static final String API_URL = "https://dubhack.dubsmash.com";
     public static final String CLIENT_ID = "rM7gvdLe6K5ZH6QC5pWWVUcHWZQYj4B3tiZyl488";
     public static final String CLIENT_SECRET = "7hNoi8DxEQ03keLq6Fg4Aqya5noTEg3VlfM6OsPzgt1imchkLTKafjKV6RXfALfEfYd77fpIxHC9m2WSqasQFqld6iwZUK3pyZF3ywVA6PxCYHcf16ugfi3BRxHkhLW3";
+    public static final String FAV_SNIPS = "/me/favorites";
+
 }
